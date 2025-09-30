cb6c5a3339d23e97632adc4048f998eb90079b82
Raksh <rakshith.nextleap@gmail.com>
2025-09-30 18:12:38 +0530

    Update STM32_Rx.ino
    added simple Receiver program to  read data and display which is received

--- STM32_Rx/STM32_Rx.ino
+++ STM32_Rx/STM32_Rx.ino
@@ -1,12 +1,28 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 
-/* to blink on bord LED of STM32F103 series*/
-void setup() {
-  pinMode(PC13, OUTPUT);
+RF24 radio(PB0, PA4); // CE, CSN on Blue Pill
+const uint64_t address = 0xF0F0F0F0E1LL;
+boolean button_state = 0;
+
+void setup() 
+{
+Serial.begin(9600);
+radio.begin();
+Serial.print("ADDRESS :");
+radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
+radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
+radio.startListening();              //This sets the module as receiver
 }
-
-void loop() {
-  digitalWrite(PC13, HIGH);
-  delay(1000);
-  digitalWrite(PC13, LOW);
-  delay(1000);
+void loop()
+{
+if (radio.available())              //Looking for the data.
+{
+Serial.println("Radio is sniffing");
+  
+char text[32] = "";                 //Saving the incoming data
+radio.read(&text, sizeof(text));    //Reading the data
+Serial.println(text);
+}
 }
