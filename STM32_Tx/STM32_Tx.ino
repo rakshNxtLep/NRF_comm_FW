1a052ecf8b1ed8cd2f422723eb4d1f296d03ca4d
Raksh <rakshith.nextleap@gmail.com>
2025-09-30 18:11:06 +0530

    Update STM32_Tx.ino
    update Tx with simple program to transmit data over particular address

--- STM32_Tx/STM32_Tx.ino
+++ STM32_Tx/STM32_Tx.ino
@@ -1,12 +1,26 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>  //need to add library in your
 
-/* to blink on bord LED of STM32F103 series*/
+RF24 radio(PB0, PA4);  // CE, CSN on Blue Pill
+const uint64_t address = 0xF0F0F0F0E1LL;
+int counter = 0;
+
 void setup() {
-  pinMode(PC13, OUTPUT);
+  Serial.begin(9600);
+  radio.begin();                   //Starting the Wireless communication
+  radio.openWritingPipe(address);  //Setting the address where we will send the data
+  radio.setPALevel(RF24_PA_MIN);   //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.stopListening();           //This sets the module as transmitter
 }
 
 void loop() {
-  digitalWrite(PC13, HIGH);
-  delay(1000);
-  digitalWrite(PC13, LOW);
-  delay(1000);
+  char text[] = " Raksh World";
+  char str[50];
+  sprintf(str, "%s %d", text, counter);
+  radio.write(&str, sizeof(str));
+
+  Serial.println(str);
+  counter++;
+  delay(2000);
 }
