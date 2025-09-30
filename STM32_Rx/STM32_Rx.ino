2fcf3dd8c6ed5264ba25c2f70111866e2894114b
Raksh <rakshith.nextleap@gmail.com>
2025-09-30 18:21:43 +0530

    V0.1 pin discription
    V0.1 with some error handling and pin connection description added.

--- STM32_Rx/STM32_Rx.ino
+++ STM32_Rx/STM32_Rx.ino
@@ -1,28 +1,73 @@
+/*
+NRF Receiver 
+₹@|<$|n
+pin configuration of 
+NRF24 to Stm32F103C
+NRF24L01 VCC ………………………………………… 3.3V of STM32F103C
+NRF24L01 CSN ………………………………………… PA4 of STM32F103C
+NRF24L01 MOSI ………………………………………… PA7 of STM32F103C
+NRF24L01 GND ………………………………………… GND of STM32F103C
+NRF24L01 CE ………………………………………… PB0 of STM32F103C
+NRF24L01 SCK ………………………………………… PA5 of STM32F103C
+NRF24L01 MISO ………………………………………… PA6 of STM32F103C
+
+*/
+
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(PB0, PA4); // CE, CSN on Blue Pill
-const uint64_t address = 0xF0F0F0F0E1LL;
-boolean button_state = 0;
+RF24 radio(PB0, PA4); // CE, CSN on Blue Pill spi1 connection
 
+const uint64_t address = 0xF0F0F0F0E1LL;
+int count=0;
+// /*
+bool ledState = false;   // Variable to store the current state of the LED (false = off, true = on)
+// */
 void setup() 
 {
 Serial.begin(9600);
 radio.begin();
 Serial.print("ADDRESS :");
+Serial.println(address);
 radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
 radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
 radio.startListening();              //This sets the module as receiver
+int count=0;
+pinMode(PC13, OUTPUT);
 }
 void loop()
 {
 if (radio.available())              //Looking for the data.
 {
+  //actual sniffing code
 Serial.println("Radio is sniffing");
-  
 char text[32] = "";                 //Saving the incoming data
 radio.read(&text, sizeof(text));    //Reading the data
+Serial.print("Received: ");
 Serial.println(text);
+// delay(100);
+count = 0;
+// /*
+  // Toggle the LED state
+    ledState = !ledState;
+    // Write the new LED state to the pin
+  digitalWrite(PC13, ledState);
+  // */
 }
+// /*
+else
+  {
+  // Serial.println(count);
+  delay(100);
+    count = count+1;
+    if(count == 50) //100 * 30 3000 dealy
+    {
+    Serial.println("not available");
+   count = 0;
+    }   
+    
+  }
+// */
 }
--- STM32_Tx/STM32_Tx.ino
+++ STM32_Tx/STM32_Tx.ino
@@ -1,26 +1,54 @@
+/*
+NRF Transmitter
+₹@|<$|n
+pin configuration of 
+NRF24 to Stm32F103C
+NRF24L01 VCC ………………………………………… 3.3V of STM32F103C
+NRF24L01 CSN ………………………………………… PA4 of STM32F103C
+NRF24L01 MOSI ………………………………………… PA7 of STM32F103C
+NRF24L01 GND ………………………………………… GND of STM32F103C
+NRF24L01 CE ………………………………………… PB0 of STM32F103C
+NRF24L01 SCK ………………………………………… PA5 of STM32F103C
+NRF24L01 MISO ………………………………………… PA6 of STM32F103C
+
+*/
+
 #include <SPI.h>
 #include <nRF24L01.h>
-#include <RF24.h>  //need to add library in your
+#include <RF24.h>
 
-RF24 radio(PB0, PA4);  // CE, CSN on Blue Pill
+RF24 radio(PB0, PA4); // CE, CSN on Blue Pill 
 const uint64_t address = 0xF0F0F0F0E1LL;
 int counter = 0;
 
-void setup() {
-  Serial.begin(9600);
-  radio.begin();                   //Starting the Wireless communication
-  radio.openWritingPipe(address);  //Setting the address where we will send the data
-  radio.setPALevel(RF24_PA_MIN);   //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
-  radio.stopListening();           //This sets the module as transmitter
+void setup() 
+{
+
+Serial.begin(9600);
+pinMode(PC13, OUTPUT);
+delay(100); // Wait 100ms for the nRF24L01+ module to stabilize after power-on.
+radio.begin();                  //Starting the Wireless communication
+radio.openWritingPipe(address); //Setting the address where we will send the data
+radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+radio.stopListening();          //This sets the module as transmitter
 }
 
-void loop() {
-  char text[] = " Raksh World";
-  char str[50];
-  sprintf(str, "%s %d", text, counter);
-  radio.write(&str, sizeof(str));
+void loop()
+{
 
-  Serial.println(str);
-  counter++;
-  delay(2000);
+//actual tx code
+char text[] = " Raksh World";
+char str[50];
+sprintf(str,"%s %d",text,counter);
+radio.write(&str, sizeof(str));  
+//Blink code
+// /*
+digitalWrite(PC13, HIGH);
+  delay(500);
+  digitalWrite(PC13, LOW);
+  delay(500);
+  // */
+Serial.println(str);
+counter++;
+delay(200);
 }
